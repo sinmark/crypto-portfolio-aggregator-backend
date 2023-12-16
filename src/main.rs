e0e5b2a0b9353f9d61833dfee19978316ab30312
Siniša Marković <sinisa.markovic.contact@gmail.com>
2023-12-16 23:21:49 +0100

    feat: add env configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use config::{Config, Environment};
+use dotenvy::dotenv;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Configuration {
+    pub server_addr: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    let config_ = Config::builder()
+        .add_source(Environment::default())
+        .build()
+        .unwrap();
+    let config: Configuration = config_.try_deserialize().unwrap();
+    println!("Server address: {}", config.server_addr);
 }
